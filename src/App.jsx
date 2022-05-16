e7a9bc5638f38e239811084f41bf940e50e201ce
priyanshsingh <priyansh@singh.com>
2022-05-16 10:33:13 +0530

    creating the frontend

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,36 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <h1>Animal Farm</h1>;
+  const [animals, setAnimals] = useState([]);
+
+  const search = async (q) => {
+    const response = await fetch(
+      "http://localhost:8080?" + new URLSearchParams({ q })
+    );
+    const data = await response.json();
+    setAnimals(data);
+  };
+
+  return (
+    <main>
+      <h1>Animal Farm</h1>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => {
+          search(e.target.value);
+        }}
+      />
+      <ul>
+        {animals.map((animal) => (
+          <li key={animal.id}>
+            <strong>{animal.type}</strong>
+            {animal.name}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 export default App;
